6eb6d1989028fa92b117d2f536db9bb7435e9b39
taua-almeida <tauan96@gmail.com>
2025-07-18 19:23:18 -0300

    feat: add length marker

--- test/marker.go
+++ test/marker.go
@@ -64,6 +64,7 @@ type MinItems struct {
 	// +govalid:minitems=1
 	ChanField chan int `json:"chan_field"`
 }
+
 // Custom types for enum testing
 type UserRole string
 type Priority int
@@ -119,12 +120,20 @@ type CELCrossField struct {
 	// Cross-field validation: Price must be less than MaxPrice
 	// +govalid:cel=value < this.MaxPrice
 	Price float64 `json:"price"`
-	
+
 	MaxPrice float64 `json:"max_price"`
-	
+
 	// Cross-field validation: Quantity * Price <= Budget
 	// +govalid:cel=value * this.Price <= this.Budget
 	Quantity float64 `json:"quantity"`
-	
+
 	Budget float64 `json:"budget"`
+}
+
+type Length struct {
+	// +govalid:length=7
+	PostalCode string `json:"postal_code"`
+
+	// +govalid:length=10
+	PhoneNumber string `json:"phone_number"`
 }
