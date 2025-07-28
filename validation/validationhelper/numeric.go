287b0320f5029a68104804e84330d18cca49fae6
Ferenc Zágon <ferenc.zagon@gmail.com>
2025-07-28 17:59:19 +0200

    fix numeric.go formatting problem

--- validation/validationhelper/numeric.go
+++ validation/validationhelper/numeric.go
@@ -4,10 +4,10 @@ package validationhelper
 // IsNumeric checks if a string contains only digit characters (0–9).
 func IsNumeric(s string) bool {
 	for _, ch := range s {
-			if ch < '0' || ch > '9' {
-				return false
-			}
+		if ch < '0' || ch > '9' {
+			return false
 		}
-	
+	}
+
 	return s != ""
 }
